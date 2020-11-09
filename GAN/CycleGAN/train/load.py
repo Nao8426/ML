0ff18dc0877fb1d4b5c8e2b90ad8b9bed54646a6
Nao8426 <50986868+Nao8426@users.noreply.github.com>
2020-11-09 18:14:46 +0900

    Update load.py

--- GAN/CycleGAN/train/load.py
+++ GAN/CycleGAN/train/load.py
@@ -16,12 +16,20 @@ class LoadDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, i):
         image_A = Image.open('{}/{}'.format(self.root, self.img_id_A[i][0]))
-        image_A = image_A.convert('L')
         if self.transform:
             image_A = self.transform(image_A)
         image_B = Image.open('{}/{}'.format(self.root, self.img_id_B[i][0]))
-        image_B = image_B.convert('L')
         if self.transform:
             image_B = self.transform(image_B)
 
         return image_A, image_B
+
+
+# データセットに対する処理（正規化など）
+class Trans():
+    def __init__(self):
+        self.norm = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))])
+
+    def __call__(self, image):
+        image = image.convert('L')
+        return self.norm(image)
